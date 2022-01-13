2287b3de89e051f0ec318e151fe1dee841733679
Torsten Kemps-Benedix <torsten.kemps-benedix@conzone.net>
2022-01-13 10:14:57 +0100

    Cleanup headWidget

--- frontend/app/Main.hs
+++ frontend/app/Main.hs
@@ -49,24 +49,14 @@ bootstrapJS =
 icon :: Text
 icon = "https://jimdo-storage.freetls.fastly.net/image/128522390/d7a73bb9-c9a2-42f5-ba88-0b675f05f538.png"
 
+iconW :: MonadWidget t m => m ()
+iconW = elAttr "link" ("rel" =: "shortcut icon" <> "href" =: icon <> "type" =: "image/x-icon") blank
+
 headWidget :: MonadWidget t m => m ()
 headWidget = do
   elAttr "meta" ("charset" =: "utf-8") blank
-  elAttr
-    "meta"
-    ( "name" =: "viewport"
-        <> "content" =: "width=device-width, initial-scale=1, shrink-to-fit=no"
-    )
-    blank
-  elAttr
-    "link"
-    ( "rel" =: "stylesheet"
-        <> "href" =: "https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
-        <> "integrity" =: "sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh"
-        <> "crossorigin" =: "anonymous"
-    )
-    blank
-  elAttr "link" ("rel" =: "shortcut icon" <> "href" =: icon <> "type" =: "image/x-icon") blank
+  elAttr "meta" ("name" =: "viewport" <> "content" =: "width=device-width, initial-scale=1, shrink-to-fit=no") blank
+  iconW
   bootstrapCSS
   bootstrapJS
   el "title" $ text "TODO App"
